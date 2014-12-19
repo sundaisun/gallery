6b01b61476ea11301dbbaf90bf785bebce6196ef
Sun Dai <sundaisf@gmail.com>
2014-12-19 15:05:21 -0800

    use php, others stay consistent with master

---
+++ index.php
@@ -0,0 +1,211 @@
+<?php include("parts/header.php"); ?>
+    <section>
+        <div class="landing-grids" data-equalizer>
+           <div class="row">
+            <article data-equalizer-watch>
+                <div class="half small-12 medium-6 columns" data-equalizer>
+                    <div class="small-12 columns img-left wow fadeInUp" data-equalizer-watch>
+                            <div class="small-12 medium-6 columns first">
+                                <a href="#"><img src="images/walmart.jpg"></a>
+                            </div>
+                            <div class="small-12 medium-6 columns last">
+                               <div class="desc">
+                                   <div class="desc-inner">
+                                    <h3>work</h3>
+                                    <a href="#"><h1>Black Friday</h1></a>
+                                   </div>
+                                </div><!--desc-->
+                            </div>
+                    </div>
+                    <!--image left-->
+                    <div class="small-12 columns img-right wow fadeInUp" data-equalizer-watch>
+                        <div class="small-12 medium-6 columns first">
+                              <a href=""><img src="images/mccafe.png"></a></div>
+                        <div class="small-12 medium-6 columns last">
+                            <div class="desc">
+                                 <div class="desc-inner">
+                                <h3>work</h3>
+                                <a href="#"><h1>White Chocolate Mocha</h1></a>
+                               </div>
+                            </div><!--desc-->
+                        </div>
+                    </div>
+                    <!--image right-->
+                </div>
+                <!--half-->
+            </article>
+
+            <article data-equalizer-watch>
+                <div class="small-12 medium-6 columns full wow fadeInUp">
+
+                   
+
+                    <div class="overlay">
+                      <div class="overlay-inner">
+                       <h3>case study</h3>
+                        <h1>New York Tristate Website</h1>
+                        <p>McDonald's Total Market. For featured content, we can add a short blug with a CTA button. We can also mention awards won, who the client was etc.</p>
+                      </div>
+                    </div>
+                    <img src="images/tristate.jpg">
+                    
+                </div>
+                <!--full-->
+            </article>
+            </div>
+            <!-- ===========ROW ONE================ -->
+        <div class="row">
+            <article data-equalizer-watch>
+                <div class="small-12 medium-6 columns full wow fadeInUp">
+
+        
+                   <div class="overlay">
+                        <div class="overlay-inner">
+                       <h3>case study</h3>
+                        <h1>CDC Protect Family</h1>
+                        <p>For featured content, we can add a short blug with a CTA button. We can also mention awards won, who the client was etc.</p>
+                      </div>
+                   </div>
+                   <img src="images/cdc.jpg">
+                </div>
+                <!--full-->
+            </article>
+
+            <article data-equalizer-watch>
+                <div class="half small-12 medium-6 columns" data-equalizer>
+                    <div class="small-12 columns text-only wow fadeInUp" data-equalizer-watch>
+
+                        <div class="desc">
+                              <div class="desc-inner">
+                                <h3>Insight</h3>
+                                <a href="#"><h1>Most likely to be a blog post</h1></a>
+                                <p>Here we can include an excerpt of some blog post our people did to show that we understand the market, the approach etc.</p>
+                               </div>
+                            </div><!--desc-->
+                           
+                    </div>
+                    <!--text only-->
+                    <div class="small-12 columns img-left wow fadeInUp" data-equalizer-watch>
+                       <div class="small-12 medium-6 columns first">
+                            <a href="#"><img src="images/coke.png"></a>
+                        </div>
+                        <div class="small-12 medium-6 columns last">
+                        <div class="desc">
+                                <div class="desc-inner">
+                                <h3>work</h3>
+                                <a href="#"><h1>Coca-Cola Wakiki</h1></a>
+                               </div>
+                            </div><!--desc-->
+                        </div>
+                        
+                    </div>
+                    <!--img left-->
+                </div>
+                <!--half-->
+            </article>
+            </div>
+            <!-- ===========ROW TWO================ -->
+
+           <div class="row">
+            <article data-equalizer-watch>
+                <div class="half small-12 medium-6 columns" data-equalizer>
+                    <div class="small-12 columns img-right wow fadeInUp" data-equalizer-watch>
+                       <div class="small-12 medium-6 columns first ">
+                            <a href="#"><img src="images/fifa.gif"></a>
+                        </div>
+                        <div class="small-12 medium-6 columns last">
+                            <div class="desc">
+                                <div class="desc-inner">
+                                <h3>work</h3>
+                                <a href="#"><h1>FIFA World Cup</h1></a>
+                               </div>
+                            </div><!--desc-->
+                        </div>
+                        
+                    </div>
+                    <!--image right-->
+                    <div class="small-12 columns img-left wow fadeInUp" data-equalizer-watch>
+                       <div class="small-12 medium-6 columns first">
+                            <a href="#"><img src="images/freecoffee.jpg"></a>
+                        </div>
+                        <div class="small-12 medium-6 columns last ">
+                        <div class="desc">
+                                 <div class="desc-inner">
+                                <h3>IW culture</h3>
+                                <a href="#"><h1>Free Coffee Event</h1></a>
+                               </div>
+                            </div><!--desc-->
+                        </div>
+                        
+                    </div>
+                    <!--image left-->
+                </div>
+                <!--half-->
+            </article>
+
+            <article data-equalizer-watch>
+                <div class="small-12 medium-6 columns full wow fadeInUp">
+                  
+                   <div class="overlay">
+                        <div class="overlay-inner">
+                       <h3>case study</h3>
+                        <h1>Los Angeles County Fair</h1>
+                        <p>For featured content, we can add a short blug with a CTA button. We can also mention awards won, who the client was etc.</p>
+                      </div>
+                   </div>
+                   <img src="images/lacf.jpg">
+                </div>
+                <!--full-->
+            </article>
+            </div>
+            <!-- ===========ROW THREE================ -->
+
+        </div>
+        <!--landing grids-->
+    </section>
+    <footer>
+        <div class="footer-btm">
+            <div class="row">
+                <div class="small-12 columns">
+                    <ul>
+                        <li><a href="#" class="twitter"><i class="fa fa-twitter-square"></i></a>
+                        </li>
+                        <li><a href="#" class="facebook"><i class="fa fa-facebook-square"></i></a>
+                        </li>
+                        <li><a href="#" class="linkedin"><i class="fa fa-linkedin-square"></i></a>
+                        </li>
+                        <li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <p>2015 IW Group, Inc. All rightes reserved.</p>
+            </div>
+        </div>
+    </footer>
+
+
+
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+    <script src="js/wow.js"></script>
+    <script>
+        var wow = new WOW(
+            {
+                offset:200
+            });
+        wow.init();
+    </script>
+    <script src="js/typed.js"></script>
+    <script>
+  $(function(){
+      $(".typed").typed({
+        strings: ["does <strong>this</strong> and <strong>that</strong> and total market stuff"],
+        typeSpeed: 60,
+        showCursor:false
+      });
+  });
+</script>
+</body>
+
+</html>
---
+++ parts/header.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gallery</title>
+    <meta name="description" content="Total market advertising agency.">
+    <meta name="keywords" content="IW,advertising,Asian,PR">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="author" content="Sun Dai">
+    <meta property="og:site_name" content="IW Group">
+    <meta property="og:title" content="IW Group">
+    <meta property="og:description" content="Total market advertising agency">
+
+
+    <link rel="stylesheet" href="stylesheets/app.css" />
+    <link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/5c5a5723-82d0-4ca5-b695-f38dcb91eca3.css" />
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="stylesheets/vendor/animate.css">
+    <script src="bower_components/modernizr/modernizr.js"></script>
+</head>
+
+<body>
+   <header>
+      <nav class="top-bar" data-topbar role="navigation">
+          <ul class="title-area">
+            <li class="name">
+              <h1><a href="#"></a></h1>
+            </li>
+            <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+          </ul>
+
+  <section class="top-bar-section">
+    <!-- Left Nav Section -->
+    <ul class="left">
+         <li class="active"><a href="#">Home</a></li>
+         <li><a href="#">Clients</a></li>
+         <li><a href="#">Work</a></li>
+         <li><a href="#">Contact</a></li>
+    </ul>
+  </section>
+        </nav>
+    <div class="row header">
+        <div class="large-12 columns">
+            <h1>IW Group is a company that<span> does <strong>this</strong> and <strong>that</strong> and total market stuff</span></h1>
+            <h1 class="typed"></h1>
+        </div>
+    </div>
+<!--
+    <video autoplay loop muted id="vid-bg">
+            <source src="video/Slowmotiondrop.webm" type="video/webm"></source>
+            <source src="video/Slowmotiondrop.mp4" type="video/mp4"></source>
+    </video>
+-->
+   </header>
+    <!--HEADER-->
