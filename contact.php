8fbd7efac22d18c98ba16c5fd1ff5fcec971f684
Sun Dai <sundaisf@gmail.com>
2014-12-22 17:32:48 -0800

    added inner page, contact mapp

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php $active="contact"; include("parts/header.php"); ?>
+   </header>
+    <!--HEADER-->
+<div id="contact-wrapper">
+<section>
+       <div class="row intro">
+        <h2>Contact</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab nostrum quo, dolorem hic porro.</p>
+        </div>
+        <div id="contact-map">
+            <iframe src="https://mapsengine.google.com/map/embed?mid=zCQdS7kxy0Fs.kpRJpU2qsn78" width="640px" height="480px"></iframe>
+
+        </div>
+
+</section>
+</div>
+<?php include("parts/footer.php");  ?>
